64758605cc9b5ec67b25a1206bbe03a3bd92d96d
Kosh1jo <koshijo34@gmail.com>
2023-10-03 13:37:17 +0700

    Babel-plugin and Customize-cra and prettier

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalSytles from '~/components/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <GlobalSytles>
+            <App />
+        </GlobalSytles>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
